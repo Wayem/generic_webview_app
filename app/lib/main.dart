99dc8d9298ef0d79942f7877eb4e8b7ace281436
Wayem <pare.maxime@outlook.com>
2025-08-06 17:56:55 +0200

    feat: add Firebase messaging setup

---
+++ app/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  await FirebaseMessaging.instance.requestPermission();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse('https://example.com'));
+
+    return MaterialApp(
+      title: 'Generic Webview App',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Generic Webview App')),
+        body: WebViewWidget(controller: controller),
+      ),
+    );
+  }
+}
