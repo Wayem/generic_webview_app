7b8e3e9053033228cec26faf1d5e0e8b07806a02
Wayem <pare.maxime@outlook.com>
2025-08-07 17:09:47 +0200

    works !

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -12,23 +12,57 @@ Future<void> main() async {
   await Firebase.initializeApp();
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   await FirebaseMessaging.instance.requestPermission();
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..loadRequest(Uri.parse('https://example.com'));
+  State<MyApp> createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
+  late final WebViewController _controller;
+  String? _fcmToken;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialisation WebView
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse('https://www.gardetameuf.com'));
+
+    // Récupération du token FCM
+    FirebaseMessaging.instance.getToken().then((token) {
+      setState(() {
+        _fcmToken = token;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Generic Webview App',
       home: Scaffold(
         appBar: AppBar(title: const Text('Generic Webview App')),
-        body: WebViewWidget(controller: controller),
+        body: Column(
+          children: [
+            if (_fcmToken != null)
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SelectableText(
+                  'FCM Token:\n$_fcmToken',
+                  style: const TextStyle(fontSize: 12),
+                ),
+              ),
+            Expanded(child: WebViewWidget(controller: _controller)),
+          ],
+        ),
       ),
     );
   }
